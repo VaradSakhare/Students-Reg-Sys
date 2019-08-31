f194c7b5543475d2b6e71b69451d1dcf8fd84ab0
VaradSakhare <varadsakhare2072001@gmail.com>
2019-09-01 00:10:17 +0530

    New Code

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,31 +1,43 @@
 package com.jetbrains;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import static javafx.scene.input.KeyCode.G;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         JFrame f1=new JFrame("My Frame");
+        JLabel l4 = new JLabel();
+        l4.setIcon(new ImageIcon("G:\\Student.jpg"));
         JLabel l1=new JLabel("Students Login");
         JLabel l2=new JLabel("Name");
         JLabel l3=new JLabel("Password");
+
         final JTextField t1=new JTextField();
         final JPasswordField p1=new JPasswordField();
         JButton b1=new JButton("Login");
         JButton b2=new JButton("Cancel");
+
         l1.setFont(new Font("Cambria",Font.BOLD,21));
         l2.setFont(new Font("Cambria",Font.BOLD,20));
         l3.setFont(new Font("Cambria",Font.BOLD,20));
-        l1.setBounds(170,130,150,20);
-        l2.setBounds(100,180,70,20);
-        l3.setBounds(100, 230,100,20);
-        t1.setBounds(200,180,90,25);
-        p1.setBounds(200,230,90,25);
-        b1.setBounds(95,300,80,25);
-        b2.setBounds(210,300,90,25);
+        l1.setBounds(140,230,150,20);
+
+        l2.setBounds(100,260,70,20);
+        t1.setBounds(200,260,90,25);
+
+        l3.setBounds(100, 320,100,20);
+        p1.setBounds(200,320,90,25);
+
+        l4.setBounds(100,50,200,170);
+
+        b1.setBounds(100,370,80,25);
+        b2.setBounds(200,370,90,25);
+        f1.add(l4);
         f1.add(l1);
         f1.add(l2);
         f1.add(t1);
@@ -34,7 +46,7 @@ public class Main {
         f1.add(b1);
         f1.add(b2);
         f1.setLayout(null);
-        f1.setSize(500,500);
+        f1.setSize(800,800);
         b1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
